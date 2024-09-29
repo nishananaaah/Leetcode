53927f55137607b0cd3816332a306fb40f01c225
nishananaaah <nishanaf731@gmail.com>
2024-09-29 18:16:06 +0530

    uppercase

---
+++ uppercase.js
@@ -0,0 +1,10 @@
+const detectCapitalUse = function(word) {
+    if(word.toLowerCase() == word || word.toUpperCase() == word) 
+        return true;
+    else if((word[0] + word.slice(1).toLowerCase()) == word) 
+        return true 
+    else 
+        return false
+}
+const word="USA"
+console.log(detectCapitalUse(word));
