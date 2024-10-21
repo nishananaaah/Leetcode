6685feb955ec34280e29cb8a2e7b35f188b820df
nishananaaah <nishanaf731@gmail.com>
2024-10-21 18:29:29 +0530

    1903. Largest Odd Number in String

---
+++ largeoddnum.js
@@ -0,0 +1,10 @@
+var largestOddNumber = function(num) {
+    for(let i = num.length - 1; i >= 0; i--) {
+        if(parseInt(num[i]) % 2 != 0) {
+            return num.substring(0, i + 1)
+        }
+    }
+    return ""
+};
+const num ="52";
+console.log(largestOddNumber(num))
