17aecbe00852fa0788730b507ee74f0fc35ad939
nishananaaah <nishanaf731@gmail.com>
2024-11-15 11:48:34 +0530

    1832. Check if the Sentence Is Pangram

---
+++ checkpanagram.js
@@ -0,0 +1,16 @@
+const  sentence = "thequickbrownfoxjumpsoverthelazydog"
+var checkIfPangram = function(sentence) {
+    let abc='abcdefghijklmnopqrstuvwxyz';
+    let count=0;
+   for(let i=0;i<abc.length;i++){
+       if(sentence.includes(abc[i])){
+        count++
+       }
+    }
+    if(count>25){
+        return true
+    }else{
+        return false
+    }
+};
+console.log(checkIfPangram(sentence))
