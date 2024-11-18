9e1a72c5486d83fb620ec044bed26f7959a25970
nishananaaah <nishanaf731@gmail.com>
2024-11-18 10:07:45 +0530

    2828. Check if a String Is an Acronym of Words

---
+++ acronymwords.js
@@ -0,0 +1,10 @@
+const words = ["alice","bob","charlie"];
+const s = "abc"
+var isAcronym = function(words, s) {
+     for(let i=0;i<words.length;i++){
+        if(words.length!=s.length) return false;
+        if(!words[i].startsWith(s[i]))return false;
+     }
+     return true;
+};
+console.log(isAcronym(words, s))
