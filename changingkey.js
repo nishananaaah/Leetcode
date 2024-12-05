11b460fbfc213d67fa324f3f78f22f5650930f9b
nishananaaah <nishanaf731@gmail.com>
2024-12-05 17:18:08 +0530

    3019. Number of Changing Keys

---
+++ changingkey.js
@@ -0,0 +1,11 @@
+const s = "aAbBcC"
+var countKeyChanges = function(s) {
+  let count = 0 ;
+  for(let i=0;i<s.length-1;i++){
+      if(s[i].toUpperCase()!=s[i+1].toUpperCase()){
+          count++;
+      }
+  }
+  return count;
+  };
+console.log(countKeyChanges(s));
