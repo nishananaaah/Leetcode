eb4638a919495d6b9afa85533a82bf12f69cee25
nishananaaah <nishanaf731@gmail.com>
2024-12-16 15:42:51 +0530

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ largestpositiveint.js
@@ -0,0 +1,14 @@
+const nums =[-1,10,6,7,-7,1]
+var findMaxK = function(nums) {
+    nums.sort((a,b)=>b-a);
+    for(let i=0;i<nums.length;i++){
+        if(nums.includes(-nums[i])){
+            return nums[i]
+        }
+    }
+    return -1
+      
+       
+   };
+   console.log(findMaxK(nums))
+   
